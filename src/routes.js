2f3e4ffadcead3fa4709ca444485dc567e7818b9
Wander Souza <wanderrodrigues_@outlook.com>
2021-03-26 18:03:24 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
+//  express biblioteca para criar o servidor
 const express = require('express');
+//  Router vai criar as rotas
 const routes = express.Router();
 
-// request, response
-routes.get('/', (request,response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+const views = __dirname + "/views/"
 
-routes.get('/job', (request, response) => {
+// request, reponse
+
+routes.get('/', (req,res) => res.render(views + "index"))
+routes.get('/job', (req,res) => res.render(views + "job"))
+routes.get('/job/edit', (req,res) => res.render(views + "job-edit"))
+routes.get('/profile', (req,res) => res.render(views + "profile"))
+
     
-})
+
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,14 +2,11 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+// usando template engine
+server.set('view engine', 'ejs')
 
 // habilitar arquivos statics
 server.use(express.static("public"))
-
-// request, reponse
-server.get('/', (request, response) => {    
-    return response.sendFile(__dirname + "/views/index.html")
-})
 
 // routes
 server.use(routes)
