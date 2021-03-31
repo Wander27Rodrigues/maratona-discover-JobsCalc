e45170bdbd65ef03d58e0c2f196c79398e6e9dff
Wander Souza <wanderrodrigues_@outlook.com>
2021-03-31 11:58:15 -0300

    routes-jobs

--- src/routes.js
+++ src/routes.js
@@ -7,17 +7,25 @@ const views = __dirname + "/views/"
 
 const profile = {
     name: "wander",
-    avatar: "https://avatars.githubusercontent.com/u/48796830?v=4",
+    avatar: "http://github.com/wander27Rodrigues.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
     "vacation-per-year": 4,
 }
 
+// array - agrupando dados
+const jobs = []
+
 // request, reponse
 
 routes.get('/', (req,res) => res.render(views + "index"))
 routes.get('/job', (req,res) => res.render(views + "job"))
+routes.post('/job', (req,res) => {
+    //req.body = {name: 'Wander Alisson Rodrigues Souza','daily-hours': '2','total-hours': '2'}
+    jobs.push(req.body)
+    return res.redirect('/')
+})
 routes.get('/job/edit', (req,res) => res.render(views + "job-edit"))
 routes.get('/profile', (req,res) => res.render(views + "profile", { profile }))
 
