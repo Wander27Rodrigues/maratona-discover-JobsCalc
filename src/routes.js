f6097ed6dd6e190bb207c9b1cd95ac1509f715f7
Wander Souza <wanderrodrigues_@outlook.com>
2021-04-01 16:29:42 -0300

    cards-ejs

--- src/routes.js
+++ src/routes.js
@@ -15,15 +15,40 @@ const profile = {
 }
 
 // array - agrupando dados
-const jobs = []
+const jobs = [
+    {
+    id: 1,
+    name: "Pizza Guloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now()
+    },
+    {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now()   
+    }
+]
 
 // request, reponse
 
-routes.get('/', (req,res) => res.render(views + "index"))
+routes.get('/', (req,res) => res.render(views + "index", { jobs }))
 routes.get('/job', (req,res) => res.render(views + "job"))
 routes.post('/job', (req,res) => {
     //req.body = {name: 'Wander Alisson Rodrigues Souza','daily-hours': '2','total-hours': '2'}
-    jobs.push(req.body)
+    
+    // logica and/Or
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // atribuindo data de hoje
+    })
     return res.redirect('/')
 })
 routes.get('/job/edit', (req,res) => res.render(views + "job-edit"))
